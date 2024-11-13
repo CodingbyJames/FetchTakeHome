5f476b7ba54081d58292e4c2063ecb3cd189db1f
James Garcia <codingbyjames@gmail.com>
2024-11-13 07:48:38 -0800

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by James Garcia on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
